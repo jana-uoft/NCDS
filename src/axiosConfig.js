b7bd728ac3ecf1bb7bb161b89c427d4c6526d703
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2018-07-08 17:16:39 -0400

    Debug authorization header not being sent in production

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -5,15 +5,21 @@ export const client = axios.create({
   responseType: 'json',
 })
 
+console.log('IN AXIOS CONFIG');
+
 export const middlewareConfig = {
   interceptors: {
     request: [
       function ({getState, dispatch, getSourceAction}, req) {
+        console.log('getState().auth.token', getState().auth.token);
+
         if (getState().auth.token) {
           req.headers['Authorization'] = getState().auth.token
           req.headers['CHECKING_IF_CAN_GET_TOKEN'] = 'YES CAN GET TOKEN'
         }
         req.headers['CHECKING_IF_CAN_ADD_HEDER'] = 'YES CAN ADD NEW HEADER'
+        console.log('req.headers', req.headers);
+
         return req
       }
     ]
