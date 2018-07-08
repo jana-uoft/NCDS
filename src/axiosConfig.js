1d6dad2b64223500238d3165a7ae875103f823fd
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2018-07-08 14:25:09 -0400

    Add console log to check NODE_ENV in server

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
 export const client = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? 'http://localhost:5000/api' : 'http://localhost:5001/api',
